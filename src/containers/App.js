9dad6ff44d35ee5117d9e37cdfef323cad81ec34
AKSHAY ASHOK <akshay.ashok@ignitarium.com>
2019-05-06 11:18:03 +0530

    Rendering mutiple jsx elements
    react

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,34 +1,49 @@
 import React, { Component } from 'react';
-import classe from './App.css';
-import Person from '../components/Persons/Person/Person';
-import ErrorBoundary from '../ErrorBoundary/ErrorBoundary'
-import Persons from '../components/Persons/Persons'
-import Cockpit from '../components/cockpit/cockpit'
+
+import classes from './App.css';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
+
 class App extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-    console.log('App[js] constructor')
-    this.state={
-      persons: [
-        { id: 'asfa1', name: 'Max', age: 28 },
-        { id: 'vasdf1', name: 'Manu', age: 29 },
-        { id: 'asdf11', name: 'Stephanie', age: 26 }
-      ],
-      otherState: 'some other value',
-      showPersons: false
-    }
-  }
-  
-  static getDerivedStateFromProps(props,state){
-   console.log('[App.js] get derivedstate from props',props)
-    return state
+    console.log('[App.js] constructor');
   }
 
-  componentDidMount(){
-    console.log("component di mount ")
+  state = {
+    persons: [
+      { id: 'asfa1', name: 'Max', age: 28 },
+      { id: 'vasdf1', name: 'Manu', age: 29 },
+      { id: 'asdf11', name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false,
+    showCockpit: true
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
   }
-  nameChangedHandler = ( event, id ) => {
+
+  // componentWillMount() {
+  //   console.log('[App.js] componentWillMount');
+  // }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
+  }
+
+  nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
     });
@@ -44,52 +59,54 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState( {persons: persons} );
-  }
+    this.setState({ persons: persons });
+  };
 
-  deletePersonHandler = (personIndex) => {
+  deletePersonHandler = personIndex => {
     // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-    this.setState({persons: persons});
-  }
+    this.setState({ persons: persons });
+  };
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState( { showPersons: !doesShow } );
-  }
+    this.setState({ showPersons: !doesShow });
+  };
 
-  render () {
-   
-    console.log("[app.js] render method")
+  render() {
+    console.log('[App.js] render');
     let persons = null;
-  
-    if ( this.state.showPersons ) {
+
+    if (this.state.showPersons) {
       persons = (
-        <div>
-          <Persons 
+        <Persons
           persons={this.state.persons}
           clicked={this.deletePersonHandler}
           changed={this.nameChangedHandler}
-          />
-         
-        </div>
+        />
       );
- 
     }
 
     return (
-
-<div className={classe.App}>
-        <Cockpit 
-        title={this.props.title} 
-        persons={this.state.persons}
-        showPersons={this.state.showPersons}
-        clicked={this.togglePersonsHandler} />
+      <div className={classes.App}>
+        <button
+          onClick={() => {
+            this.setState({ showCockpit: false });
+          }}
+        >
+          Remove Cockpit
+        </button>
+        {this.state.showCockpit ? (
+          <Cockpit
+            title={this.props.appTitle}
+            showPersons={this.state.showPersons}
+            personsLength={this.state.persons.length}
+            clicked={this.togglePersonsHandler}
+          />
+        ) : null}
         {persons}
       </div>
-
-      
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
